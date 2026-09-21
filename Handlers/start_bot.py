f0a799f84fee8e5bebc8712b9f24b78f04b9ac17
kirill <kirill483823@gmail.com>
2026-09-21 17:48:39 +0400

    олваола

--- Handlers/start_bot.py
+++ Handlers/start_bot.py
@@ -15,7 +15,8 @@ async def startBot(message: Message, state: FSMContext):
     if message.from_user.id in ADMINS_IDS:
         await state.set_state(CreatePosts.CreateP)
         await message.answer("👋 Привет, ты находишься в боте для создания и редактирования постов для Telegram-канала"
-        "\n\n🔩 Бот находится в разработке, так что при обнаружении багов или ошибок сообщи мне @kirill_r24",
+        "\n\n🔩 Бот находится в разработке, так что при обнаружении багов или ошибок сообщи мне @kirill_r24"
+        "\n\n Бот не умее работать с видио и с несколькими медиа, так что в посте должно быть одно фото"
         reply_markup=create_post_keyb)
     else:
         await message.answer("Ты не админ")
